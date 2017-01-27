3becde076a4933d20584df4c06b00e646f5e28d4
Facundo Balboa <monoflauta@gmail.com>
2017-01-27 12:10:30 -0300

    Ya se puede mover el sello y soltar. Fucking rigidbodiessss! yeaaah

--- DaVinci Game Jam/Assets/Scripts/Office/HandController.cs
+++ DaVinci Game Jam/Assets/Scripts/Office/HandController.cs
@@ -4,13 +4,84 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour {
 
-	// Use this for initialization
+    public LayerMask layerMaskBase;
+    public LayerMask layerMaskSeal;
+    public GameObject seal;
+
+    private Vector3 _mousePosition;
+    private bool _movingSeal;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        // Updates the position of the mouse on the desk
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, layerMaskBase))
+        {
+            _mousePosition = hit.point;
+        }
+
+        //Check input
+        if(Input.GetMouseButtonDown(0))
+            PressClick();
+
+        //If needed, moves the seal
+        if(_movingSeal)
+        {
+            seal.transform.position = _mousePosition + Vector3.up * 0.7f;
+            seal.transform.forward = Vector3.up;
+        }
 	}
+
+    /// <summary>
+    /// Called when user makes left click.
+    /// </summary>
+    private void PressClick()
+    {
+        if(_movingSeal)
+        {
+            ActivateSeal(false);
+        }
+        else
+        {
+            var col = Physics.OverlapSphere(_mousePosition, 0.5f, layerMaskSeal);
+            for (int i = col.Length - 1; i >= 0; i--)
+            {
+                if (col[i] == seal.GetComponent<Collider>())
+                {
+                    ActivateSeal(true);
+                    
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Activates or desactivates the dragging of the seal.
+    /// </summary>
+    /// <param name="value">If you want to activate or desactivate</param>
+    private void ActivateSeal(bool value)
+    {
+        _movingSeal = value;
+        if (value)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (_mousePosition != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(_mousePosition, 0.5f);
+        }
+    }
 }
