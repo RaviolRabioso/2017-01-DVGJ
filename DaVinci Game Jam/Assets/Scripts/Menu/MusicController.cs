2be7de01aaf872b7a356096e9702014a254a0970
Facundo Balboa <monoflauta@gmail.com>
2017-01-28 16:20:08 -0300

    Agregué la música que se reproduce pero si entras desde el menu

---
+++ DaVinci Game Jam/Assets/Scripts/Menu/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    public static MusicController instance;
+
+	// Use this for initialization
+	void Start () {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
