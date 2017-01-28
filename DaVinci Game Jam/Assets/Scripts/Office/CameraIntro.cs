2604a6000f65696243b3c5bd9e764ca75c4ef214
Exequiel Blanco <eblancodevelop@gmail.com>
2017-01-28 17:25:27 -0300

    Agregada Intro de camara

---
+++ DaVinci Game Jam/Assets/Scripts/Office/CameraIntro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraIntro : MonoBehaviour 
+{
+	public Transform start;
+	public Transform end;
+	public float duration;
+	void Start () 
+	{
+		iTween.ValueTo(gameObject, iTween.Hash("from", 0, "to", 1, "onupdate", "Lerp", "time", duration, "easetype", iTween.EaseType.easeOutExpo));
+	}
+
+	void Lerp(float f)
+	{
+		transform.position = Vector3.Lerp (start.position, end.position, f);
+		var q = transform.rotation;
+		q.eulerAngles = Vector3.Lerp (start.rotation.eulerAngles, end.rotation.eulerAngles, f);
+		transform.rotation = q;
+	}
+}
