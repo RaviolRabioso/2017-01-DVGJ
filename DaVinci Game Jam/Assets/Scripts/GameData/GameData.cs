82b08f54880959162e0c8f099a514c8d20e073f3
Exequiel Blanco <eblancodevelop@gmail.com>
2017-01-29 18:05:48 -0300

    Varios #Shits nuevos

--- DaVinci Game Jam/Assets/Scripts/GameData/GameData.cs
+++ DaVinci Game Jam/Assets/Scripts/GameData/GameData.cs
@@ -41,38 +41,38 @@ public class GameData
 
 		_questions.Add(	new Question(	"Un fiscal encontró información que podría vincularte a algo ilegal.",
 										new Answer("Hacer que se tropiece con una bala.", -2, -2, 0, 1, -3, "n|El fiscal #Prisman habria sido picado por una bala. Ampliaremos."),
-										new Answer("Filtrar video porno del fiscal.", 2, 0, 0, 0, 3),
-										new Answer("Meh, no pienso darle bola.", 0, 0, 0, 0, 1)));
+										new Answer("Filtrar video porno del fiscal.", 2, 0, 0, 0, 3,"r|Al final la gran evidencia del fiscal #Prisman esta entre sus piernas. #socotroco"),
+										new Answer("Meh, no pienso darle bola.", 0, 0, 0, 0, 1, "n|El fiscal #Prisman amenaza con denunciar al Presidente. #KB")));
 
-        _questions.Add(new Question(	"La multinacional Morfato te quiere dejar 1.000.000.000 dolares a cambio de seguir cuidando los campos", 
-							            new Answer("Que se cague el campo!", -3, 3, 2, 1, 0), 
-							            new Answer("Para nada, voy a denunciarlos!", 3, 1, -4, -1, 2), 
-							            new Answer("Paso y me hago el boludo", 0, 0, -2, 0, -1)));
+        _questions.Add(new Question(	"La multinacional Morfato te quiere dejar 1.000.000.000 dolares a cambio de seguir 'cuidando' los campos", 
+							            new Answer("Que se cague el campo!", -3, 3, 2, 1, 0, "p|El wacho de la @Ashelenn salio con 4 vrasos, debe ser por dog chau."), 
+			new Answer("Para nada, voy a denunciarlos!", 3, 1, -4, -1, 2, "i|Escandalo internacional: " + GetUserNameForShit() + " denuncia a #Morfato por coimeros."), 
+							            new Answer("Paso y me hago el boludo", 0, 0, -2, 0, -1, "e|Nada importante paso hoy.")));
 
         _questions.Add(new Question("El Gremio reclama una mejora salarial para los trabajadores",
-            new Answer("Laburen vagos!", -3, 3, 1, 0, 2),
-            new Answer("Tomen todo lo que quieran", 5, -3, -5, 0, 5),
-            new Answer("Que lo defina el congreso", -1, -1, 0, 0, -3)));
+			new Answer("Laburen vagos!", -3, 3, 1, 0, 2, "r|Los negritos se creen importantes. No son como uno. Viva " + GetUserNameForShit()),
+			new Answer("Tomen todo lo que quieran", 5, -3, -5, 0, 5, "r|"+GetUserNameForShit()+" quiere mantener a los vagos. Si sigo pagando asi voy a tener que vacacionar en Gessell #PUAJ."),
+			new Answer("Que lo defina el congreso", -1, -1, 0, 0, -3, "n|Se solicita informacion respecto a las pelotas de " + GetUserNameForShit())));
 
         _questions.Add(new Question("La economía esta por el piso y hay que elevar los impuestos",
-            new Answer("Que lo pague el pueblo, es su economía", -3, 1, 5, 0, 1),
-            new Answer("Que lo pague la clase alta, ellos se beneficiaron con la actual crisis", 3, -4, 5, 0, -3),
-            new Answer("Que lo paguen todos, menos yo obviamente", -2, -2, 5, 0, -1)));
+            new Answer("Que lo pague el pueblo, es su economía", -3, 1, 5, 0, 1, "n|Hoy te damos las 10 mejores recetas para comer carton. #YUMMI #Rico"),
+            new Answer("Que lo pague la clase alta, ellos se beneficiaron con la actual crisis", 3, -4, 5, 0, -3, "p|Los chetitos se sienten re zarpado', la villa esta de fiesta eaea"),
+            new Answer("Que lo paguen todos, menos yo obviamente", -2, -2, 5, 0, -1, "e|PROMO: 20% extra DE REGALO en cada envase de #vaselina.")));
 
         _questions.Add(new Question("Muchos innmigrantes les pareció buena idea entrar a nuestro país",
-            new Answer("Sólo queremos atletas, que aprendan a saltar muros", 2, 3, 1, -3, 1),
-            new Answer("Son todos bienvenidos!", -2, -3, -1, 3, 1),
-            new Answer("Mirá vo...", -3, -3, -2, 0, -3)));
+            new Answer("Sólo queremos atletas, que aprendan a saltar muros", 2, 3, 1, -3, 1, "r|Alguien sabe porque quedaron tan pocos albaniles?"),
+            new Answer("Son todos bienvenidos!", -2, -3, -1, 3, 1, "r|Que bueno que el Country tiene paredones electricos. #Horror"),
+            new Answer("Mirá vo...", -3, -3, -2, 0, -3, "n|ULTIMO MOMENTO: Eeeeeeeehhhhhhhhhhhh.... #Ampliaremos")));
 
         _questions.Add(new Question("Nuestro pequeño país vecino está por entrar en guerra con una potencia mundial",
-            new Answer("Que la coman, apoyamos a la potencia", -2, -1, -1, +2, +2),
-            new Answer("Son nuestros hermanos, los apoyaremos", -1, -2, -3, 3, 2),
-            new Answer("Mándele a ambos buena suerte", 0, 0, 1, -3, -1)));
+			new Answer("Que la coman, apoyamos a la potencia", -2, -1, -1, +2, +2, "i|"+GetUserNameForShit()+" decide entrar en la guerra y apoyar a #USA."),
+			new Answer("Son nuestros hermanos, los apoyaremos", -1, -2, -3, 3, 2, "i|"+GetUserNameForShit()+ "decide entrar en la guerra y apoyar a #Zukistan."),
+			new Answer("Mándele a ambos buena suerte", 0, 0, 1, -3, -1, "i|"+GetUserNameForShit()+ " acerca de la guerra: Me preocupan mas las #Piranas")));
 
 		_questions.Add(	new Question(	"Varias protestas se están generando cerca de la casa del gobierno por los últimos despidos. ¿Deberíamos detenerlos?",
-										new Answer("Que se note lo mucho que me quiere el pueblo. Plomo para todos!", -3, 1, 2, -1, -1),
-										new Answer("Recuperen ya mismo esos puestos de trabajo.", 3, -2, -3, 1, 1),
-										new Answer("Dejen que protesten, me voy de vacaciones.", -1, 0, -1, -1, 0)));
+										new Answer("Que se note lo mucho que me quiere el pueblo. Plomo para todos!", -3, 1, 2, -1, -1, "n|ALERTA METEOROLOGICO: Se esperan lluvias de plomo en el microcentro."),
+										new Answer("Recuperen ya mismo esos puestos de trabajo.", 3, -2, -3, 1, 1, "e|"+GetUserNameForShit()+" vs #empresarios. La gente gana una prorroga e indemnizaciones."),
+										new Answer("Dejen que protesten, me voy de vacaciones.", -1, 0, -1, -1, 0, "n|"+GetUserNameForShit()+" se fractura un tobillo al saltar la soga en sus vacaciones. #Ampliaremos")));
 
 		_questions.Add(	new Question(	"Transnacionales se ofrecen a invertir un buen monto de millones para usted y sus allegados, siempre en cuanto apruebe algunos despidos...",
 										new Answer("Hoy invito yo!", -2, 2, 1, -2, -1),
